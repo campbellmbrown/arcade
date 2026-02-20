aa525480676970cfbfdb08a6e6dcb8fecf75b3d6
Campbell Brown <campbellbrown093@gmail.com>
2026-02-20 20:05:51 +1300

    Make wave properties settable

--- Arcade/Utility/Wave.cs
+++ Arcade/Utility/Wave.cs
@@ -1,8 +1,19 @@
 namespace Arcade.Utility;
 
-public record Wave(float Amplitude, float Frequency, float Phase)
+public record Wave
 {
     const float TwoPi = 2 * MathF.PI;
+
+    public Wave(float amplitude, float frequency, float phase)
+    {
+        Amplitude = amplitude;
+        Frequency = frequency;
+        Phase = phase;
+    }
+
+    public float Amplitude { get; set; }
+    public float Frequency { get; set; }
+    public float Phase { get; set; }
 
     public float Value(float time)
     {
